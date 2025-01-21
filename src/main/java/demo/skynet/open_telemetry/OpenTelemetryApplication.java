30c00eb550a2384880743ef0222ceb194bd0e4a1
mymborrini <mattia.borrini@adesso.ch>
2025-01-21 16:29:27 +0100

    Starting point

---
+++ src/main/java/demo/skynet/open_telemetry/OpenTelemetryApplication.java
@@ -0,0 +1,13 @@
+package demo.skynet.open_telemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTelemetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenTelemetryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/skynet/open_telemetry/OpenTelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.skynet.open_telemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenTelemetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
